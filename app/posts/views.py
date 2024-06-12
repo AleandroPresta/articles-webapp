614fbcdc3953eb9eba42432a7158e50b2fa81324
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-12 11:10:28 +0200

    Added new post functionality

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 # Create your views here.
 def all_posts(request):
@@ -9,3 +11,16 @@ def all_posts(request):
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', { 'post': post })
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    if(request.method=='POST'):
+        form = forms.CreatePosts(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return render(request, 'posts/all_posts.html', { 'form': form })
+    else:
+        form = forms.CreatePosts()
+    return render(request, 'posts/post_new.html', { 'form': form })
