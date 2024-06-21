31115919fed6bf0a3b667e48e4760d6fbb12fe75
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-21 16:55:34 +0200

    Added new user model with proxy method and the new user page in the admin panel

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Register your models here.
+admin.site.register(UserProfile)
+
+# Mix the User model with the UserProfile model
+class UserProfileInLine(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+    
+# Extend the User model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = [ "username", "email", "first_name", "last_name", "password"]
+    inlines = [UserProfileInLine]
+    
+# Re-register UserAdmin
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+# Create automatically the user profile when a new user is created 
+# (the fields will be added by the user in his user's modification page)
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = UserProfile(user=instance)
+        user_profile.save()
+        
+# Connect the signal to the function
+post_save.connect(create_profile, sender=User)
