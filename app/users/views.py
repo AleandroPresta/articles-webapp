d7a2b460cbe6dce2666beb90519d1529ee05b1c9
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-22 14:44:49 +0200

    Added logging

--- app/users/views.py
+++ app/users/views.py
@@ -5,6 +5,20 @@ from .forms import UserInfoForm
 from .models import UserProfile
 from django.contrib import messages
 
+'''
+    Create a logger for the views
+'''
+import logging
+
+logging_path = './logs/users_views.log'
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S',
+    filename=logging_path,
+    )
+
 # Create your views here.
 def register_view(request):
     # If the request method is POST, it means the form has been submitted
@@ -44,25 +58,38 @@ def logout_view(request):
 
 
 def update_profile_view(request):
+    logging.debug('Update profile view')
+    logging.info(f'Request: {request}')
     if request.user.is_authenticated:
         try:
             current_user = UserProfile.objects.get(user=request.user)
+            logging.debug(f'Current user: {current_user}')
         except UserProfile.DoesNotExist:
+            logging.error('UserProfile does not exist.')
             messages.error(request, "UserProfile does not exist.")
+            logging.debug('Redirecting to login')
             return redirect('users:login')
 
         if request.method == 'POST':
+            logging.debug('Initializing form for POST request')
             # Include request.FILES for file uploads
             form = UserInfoForm(request.POST, request.FILES, instance=current_user)
             if form.is_valid():
+                logging.debug('Form is valid')
                 form.save()
+                logging.debug('Profile updated successfully')
                 messages.success(request, 'Profile updated successfully')
+                logging.debug('Redirecting to /')
                 return redirect('/')
         else:
             # Initialize the form with the instance but without POST data for GET requests
+            logging.debug('Initializing form for GET request')
             form = UserInfoForm(instance=current_user)
 
+        logging.debug('Rendering update_profile.html')
         return render(request, 'users/update_profile.html', {'form': form})
     else:
+        logging.error('User is not authenticated')
         messages.error(request, 'You must be logged in to update your profile')
+        logging.debug('Redirecting to login')
         return redirect('users:login')
