faf06fa30c0b7649a51b046f9ebd06d98958612d
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-22 15:48:12 +0200

    Deleted useless dir

--- app/static/js/app.js
+++
@@ -1,6 +0,0 @@
-const logoutModal = document.getElementById('logoutModal1')
-const logoutButton = document.getElementById('logout-btn')
-
-logoutModal.addEventListener('shown.bs.modal', () => {
-    logoutButton.focus()
-})
