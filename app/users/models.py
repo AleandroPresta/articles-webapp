774b6d0a2042416228383ced5565285f4d13209f
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-06-23 18:22:46 +0200

    Added bio, address and phone to user infos

--- app/users/models.py
+++ app/users/models.py
@@ -7,6 +7,10 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(default='../static/assets/user-fallback.webp', blank=True)
+    bio = models.TextField(max_length=500, blank=True, default='')
+    address = models.CharField(max_length=100, blank=True, default='')
+    phone = models.CharField(max_length=20, blank=True, default='')
+    
     
     def __str__(self):
         return self.user.username
